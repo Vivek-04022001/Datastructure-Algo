d494122114ea1c18344cfbcf1aca1d9f4c9daad4
vivek kumar <vk285090@gmail.com>
2024-05-07 07:44:52 +0530

    starting new data structure : Hashtables

--- 06_Arrays/05_MergeSortedArrays.js
+++ 06_Arrays/05_MergeSortedArrays.js
@@ -5,6 +5,7 @@ function mergedSortedArray(array1, array2) {
     let i = 0;
     let j = 0;
   
+    // comparing between 2 arrays only happen until if there is any element inside the array. if there is no element the comparing will be stop
     while (i < array1.length && j < array2.length) {
       if (array1[i] <= array2[j]) {
         mergedArray.push(array1[i]);
@@ -15,6 +16,7 @@ function mergedSortedArray(array1, array2) {
       }
     }
 
+    // Here, if there is anyone array which has no element left to compare, while loop than push the elments from the other array to the merged array
     while(i < array1.length){
         mergedArray.push(array1[i])
         i++
---
+++ 07_HashTables/1_Introduction.js
@@ -0,0 +1,8 @@
+/*
+
+- Hash tables, also known as hash maps or dictionaries in other programming languages, are a fundamental data structure used to store key-value pairs. They offer efficient insertion, deletion, and retrieval of elements based on keys.
+
+- n JavaScript, hash tables are often implemented using objects ({}) or Maps (Map). Here's a brief overview of each:
+
+
+*/ 
