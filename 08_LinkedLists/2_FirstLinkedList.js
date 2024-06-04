0d720f755468cf1d0fd2e0f20c240c48e7bb83f2
vivek kumar <vk285090@gmail.com>
2024-06-04 22:47:56 +0530

    working in progress LinkedList

---
+++ 08_LinkedLists/2_FirstLinkedList.js
@@ -0,0 +1,73 @@
+// 10 -> 5 -> 16
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    this.printList();
+  }
+
+  prepend(value) {
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
+
+  insert(index, value) {
+    // check params
+    if(index >= this.length) return this.append(value);
+    if(index === 0) return this.prepend(value);
+    if(index === this.length) return this.append(value);
+
+    let newNode = new Node(value);
+
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(12);
+
+myLinkedList.prepend(9);
