1d913d9dc143c85455ed5eee3782ea5156e08268
vivek kumar <vk285090@gmail.com>
2024-06-11 18:28:43 +0530

    Starting today stack and queues with introduction.js file

---
+++ 09_Stack&Queues/1_Introduction.js
@@ -0,0 +1,34 @@
+
+/*
+
+----------------------
+        STACKS
+----------------------
+
+Definition
+A stack is a linear data structure that follows the Last In, First Out (LIFO) principle. This means that the last element added to the stack is the first one to be removed. Think of it like a stack of plates: you add plates to the top and also remove them from the top.
+
+Operations
+push(element): Add an element to the top of the stack.
+pop(): Remove and return the top element from the stack.
+peek(): Return the top element without removing it from the stack.
+isEmpty(): Check if the stack is empty.
+size(): Return the number of elements in the stack.
+
+* Lookup -> O(n)
+* Pop, Push, Peek -> O(1)
+
+----------------------
+        Queues
+----------------------
+Definition
+A queue is a linear data structure that follows the First In, First Out (FIFO) principle. This means that the first element added to the queue is the first one to be removed. Think of it like a line of people waiting for a bus: the first person in line is the first one to board the bus.
+
+Operations
+enqueue(element): Add an element to the end of the queue.
+dequeue(): Remove and return the front element from the queue.
+front(): Return the front element without removing it from the queue.
+isEmpty(): Check if the queue is empty.
+size(): Return the number of elements in the queue.
+
+*/ 
